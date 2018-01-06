dd871283e49669db638e37e8e9a88b9b48fb1342
Anthony Duren <amduren@gmail.com>
2018-01-06 16:42:05 -0600

    Remove sample content and render tracks w/ jQuery

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -2,9 +2,8 @@ var Trackster = {};
 const API_KEY = 'aa0ed3e2abeef58b4cb8426031c9e6cb';
 
 $(document).ready(function () {
-  $('#search-button').on('click', function () {
-    var searchText = $('#search-input').val();
-    Trackster.searchTracksByTitle(searchText);
+  $('#search-button').click(function () {
+    Trackster.searchTracksByTitle($('#search-input').val());
   });
 });
 
@@ -13,7 +12,28 @@ $(document).ready(function () {
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
+  var $trackList = $('#tracks-list');
 
+  $trackList.empty();
+
+  for (var trackIndex = 0; trackIndex < tracks.length; trackIndex++) {
+    var track = tracks[trackIndex];
+    var mediumAlbumArt = track.image[1]['#text'];
+    var trackRow =
+      '<div class="row track">' +
+      '  <div class="col-xs-1 col-xs-offset-1">' +
+      '   <a href="' + track.url + '" target="_blank">' +
+      '     <i class="fa fa-play-circle-o fa-2x"></i>' +
+      '   </a>' +
+      '  </div>' +
+      '  <div class="col-xs-4">' + track.name + '</div>' +
+      '  <div class="col-xs-2">' + track.artist + '</div>' +
+      '  <div class="col-xs-2"><img src="' + mediumAlbumArt + '"/></div>' +
+      '  <div class="col-xs-2 listeners">' + track.listeners + '</div>' +
+      '</div>';
+
+      $trackList.append(trackRow);
+  }
 };
 
 /*
@@ -22,9 +42,9 @@ Trackster.renderTracks = function(tracks) {
 */
 Trackster.searchTracksByTitle = function(title) {
   $.ajax({
-    url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+    url: 'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
     success: function(response) {
-      //Trackster.renderTracks(response);
+      Trackster.renderTracks(response.results.trackmatches.track);
     }
   });
 };
